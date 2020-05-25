8b4828aa1e45f261a8ef7e1409760d5d34fba4ef
cryptodescriptor <36749544+cryptodescriptor@users.noreply.github.com>
2020-05-25 17:35:33 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// forEach polyfill
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+   NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+var links = document.querySelectorAll('.nav-link');
+
+// Set nav link to active based on window location
+
+var loc = window.location.hash.replace('#', '');
+
+if (!loc) {
+  links[0].classList.add('active');
+} else {
+  document.querySelector('a[href="#'+loc+'"]').classList.add('active');
+}
+
+// Nav link click event listener
+
+links.forEach(function(e) {
+  e.addEventListener('click', function() {
+    links.forEach(function(e) {
+      e.classList.remove('active');
+    });
+    e.classList.add('active');
+  });
+});
